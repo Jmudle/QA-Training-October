23ab14edff1da1d07dbf4b33b912f63e9da78723
jamie <jamie@email>
2023-10-02 13:30:00 +0100

    JM added code and selection app

--- October-test2.py
+++ October-test2.py
@@ -1,3 +1,3 @@
 username=input('Please enter your name')
 age=input('Please enter your age')
-print(username,'is',age,'years old')
+print('Hello', username,'you are',age,'years old')
---
+++ selection_part1.py
@@ -0,0 +1,7 @@
+age=19
+if age >= 18:
+    print('You are in category A')
+elif age == 16:
+    print('You are in Category B')
+elif age < 16:
+    print('You are in Category C')
