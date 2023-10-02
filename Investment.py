7663197841e3ed34f16d275c94a99a466ac30db1
Jmudle <146005336+Jmudle@users.noreply.github.com>
2023-10-02 17:13:06 +0100

    Update Investment.py

--- Investment.py
+++ Investment.py
@@ -1,6 +1,7 @@
 initial_investment = int(input("Please enter your starting capital:"))
 target_value = int(input("Please enter your goal:"))
 interest_rate = float(input("Please enter your rate:"))
+interest_rate = interest_rate_percentage / 100.0
 years = 0
 while initial_investment < target_value:
     initial_investment += initial_investment * interest_rate
