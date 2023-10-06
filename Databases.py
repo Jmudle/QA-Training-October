759be94f77bfaf55aa7dc196d4555de55c1a497e
Jamie <Jamie@Jamie>
2023-10-06 10:54:19 +0100

    JM: added databases.py for database queries

---
+++ Databases.py
@@ -0,0 +1,18 @@
+import pyodbc
+
+connectionString = r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=MIS;DATABASE=qastore;Trusted_Connection=yes'
+
+
+
+def dbQuery(query):
+    conn = pyodbc.connect(connectionString)
+    cur = conn.cursor()
+    result = cur.execute(query).fetchall()
+    conn.close()
+    return result
+
+#result = dbQuery("SELECT * FROM salesperson ORDER BY salary DESC")#
+result = dbQuery("SELECT * FROM salesperson WHERE county = 'Surrey'")
+
+for row in result:
+    print(row)
