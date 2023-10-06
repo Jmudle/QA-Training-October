18358e7ef0ea73086fe127156018323943a964d3
Jamie <Jamie@Jamie>
2023-10-06 15:02:32 +0100

    JM: added csv functionality to student_database_update

--- student_database_update.py
+++ student_database_update.py
@@ -2,9 +2,18 @@ import pyodbc
 
 cs = r'DRIVER={ODBC Driver 17 for SQL Server};SERVER=MIS;DATABASE=qastore;Trusted_Connection=yes'
 
-queryadd = """INSERT INTO student
-VALUES (00002, 'Davidson', 'Oscar', '14 River Way', 'Sheffield'),
-(00003, 'Peterson', 'Ragnar', '35 Sea Bank', 'Manchester');"""
+with open('studentinfo.csv', 'r') as file1:
+    result = file1.readlines()
+
+for line in result:
+    query = line.split(',')
+    sqlStr = f"""INSERT INTO student
+    VALUES ('{int(query[0])}', '{query[1]}', '{query[2]}', '{query[3]}', '{query[4]}')"""
+    conn = pyodbc.connect(cs)
+    cur = conn.cursor()
+    cur.execute(sqlStr)
+    conn.commit()
+    conn.close()
 
 queryupdate = """UPDATE student
 SET Firstname = 'Steven'
@@ -21,14 +30,14 @@ WHERE PersonID = 00003;"""
 
 #dbQuery(queryadd)
 
-def dbQuery(update):
-    conn = pyodbc.connect(cs)
-    cur = conn.cursor()
-    cur.execute(update)
+#def dbQuery(update):
+   # conn = pyodbc.connect(cs)
+    #cur = conn.cursor()
+    #cur.execute(update)
 
-    conn.commit()
+    #conn.commit()
 
-    conn.close()
+    #conn.close()
 
-dbQuery(queryupdate)
+#dbQuery(queryupdate)
     
